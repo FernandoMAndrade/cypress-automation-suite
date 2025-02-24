641efd1018e91b7a908da7d5b6509db95e70b78e
Fernando Moura Andrade <50086637+FernandoMAndrade@users.noreply.github.com>
2025-02-24 16:21:18 -0300

    feat(linter): improve linter (#1)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     supportFile: 'support/cypress/e2e.ts',
     specPattern: '**/*.spec.ts',
+    video: false,
+    screenshotOnRunFailure: false,
+    retries: {
+      runMode: 1,
+      openMode: 0,
+    },
     setupNodeEvents(on, config) {
       on('task', {
-        log: function log(message) {
-          console.log(message)
-          return null
+        log(message) {
+          // eslint-disable-next-line no-console
+          console.log(`[CYPRESS LOG]: ${message}`);
+          return null;
         },
-      })
-      return config
+      });
+      return config;
     },
   },
-})
+});
---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import cypress from 'eslint-plugin-cypress/flat';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+const tsConfig = {
+  files: ['**/*.ts'],
+  ignores: ['**/*.spec.ts'],
+  languageOptions: {
+    parser: tsparser,
+    parserOptions: { project: './tsconfig.json' },
+  },
+  plugins: { '@typescript-eslint': tseslint },
+  rules: {
+    ...tseslint.configs.recommended.rules,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'warn',
+    'eol-last': ['error', 'always'],
+  },
+};
+
+const cypressTestsConfig = {
+  files: ['**/*.spec.ts'],
+  plugins: { '@typescript-eslint': tseslint },
+  rules: {
+    'cypress/no-unnecessary-waiting': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
+
+export default [js.configs.recommended, tsConfig, cypress.configs.recommended, cypressTestsConfig];
